b0ab668a7108df462f20f649f8b6634349f1b129
Diego Vega <davegav@unal.edu.co>
2020-12-16 08:37:41 -0500

    prueba

--- src/components/User.vue
+++ src/components/User.vue
@@ -36,9 +36,8 @@
         this.salida = "Procesando!!!";
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-        const res = await axios.post("https://cajero-api123456.herokuapp.com/user/auth/", {"username": username, "password": password});
-        const dataRes = res.then((response) => response.data)
-        this.salida  = dataRes;
+        const {data:response} = await axios.post("https://cajero-api123456.herokuapp.com/user/auth/", {"username": username, "password": password});
+        this.salida  = response;
       }
     },
     created: function(){
